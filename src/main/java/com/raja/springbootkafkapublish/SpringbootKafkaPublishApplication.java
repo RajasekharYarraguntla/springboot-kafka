0762f98c297887f41b5d0d82d556467c74b22952
ryarraguntla <ryarraguntla@evoketechnologies.com>
2024-02-19 16:47:54 +0530

    initial commit

---
+++ src/main/java/com/raja/springbootkafkapublish/SpringbootKafkaPublishApplication.java
@@ -0,0 +1,13 @@
+package com.raja.springbootkafkapublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaPublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raja/springbootkafkapublish/SpringbootKafkaPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raja.springbootkafkapublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaPublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
